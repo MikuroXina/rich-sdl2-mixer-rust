9ef591b8a5b8cf9e757d95e29def2e1767bf1aeb
MikuroXina <ryosukadnak@gmail.com>
2021-08-20 23:06:56 +0900

    feat: Add play and play_fade_in

--- src/chunk/channel.rs
+++ src/chunk/channel.rs
@@ -1,8 +1,46 @@
 //! Channels to play a [`MixChunk`].
 
-use std::marker::PhantomData;
+use rich_sdl2_rust::{Result, Sdl, SdlError};
+use std::{marker::PhantomData, os::raw::c_int};
 
+use super::MixChunk;
 use crate::{bind, device::MixDevice};
+
+/// Loops on playing in [`PlayOptions`].
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PlayLoops {
+    /// Playing infinitely.
+    Infinite,
+    /// Playing only once.
+    OneShot,
+    /// Playing the specified number of times.
+    Times(u32),
+}
+
+impl PlayLoops {
+    fn into_raw(self) -> c_int {
+        match self {
+            PlayLoops::Infinite => -1,
+            PlayLoops::OneShot => 0,
+            PlayLoops::Times(n) => (n - 1) as _,
+        }
+    }
+}
+
+impl Default for PlayLoops {
+    fn default() -> Self {
+        Self::OneShot
+    }
+}
+
+/// Options to play a chunk by [`Channel::play`].
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct PlayOptions {
+    /// Loops on playing.
+    pub loops: PlayLoops,
+    /// The maximum duration of playing in milliseconds. Stopping by `loops` is prior to by `duration`.
+    pub duration: Option<u32>,
+}
 
 /// A mixing channel for playing a [`MixChunk`].
 #[derive(Debug, PartialEq, Eq)]
@@ -26,5 +64,45 @@ impl<'device> Channel<'device> {
             panic!("must allocate channels at first");
         }
         Self(-1, PhantomData)
+    }
+
+    /// Starts to play a chunk.
+    pub fn play(self, chunk: &MixChunk, options: PlayOptions) -> Result<Self> {
+        let id = unsafe {
+            bind::Mix_PlayChannelTimed(
+                self.0,
+                chunk.ptr.as_ptr(),
+                options.loops.into_raw(),
+                options.duration.map_or(-1, |d| d as _),
+            )
+        };
+        if id == -1 {
+            Err(SdlError::Others { msg: Sdl::error() })
+        } else {
+            Ok(Self(id, PhantomData))
+        }
+    }
+
+    /// Starts to play a chunk with fade-in time in milliseconds.
+    pub fn play_fade_in(
+        self,
+        chunk: &MixChunk,
+        fade_in: u32,
+        options: PlayOptions,
+    ) -> Result<Self> {
+        let id = unsafe {
+            bind::Mix_FadeInChannelTimed(
+                self.0,
+                chunk.ptr.as_ptr(),
+                options.loops.into_raw(),
+                fade_in as _,
+                options.duration.map_or(-1, |d| d as _),
+            )
+        };
+        if id == -1 {
+            Err(SdlError::Others { msg: Sdl::error() })
+        } else {
+            Ok(Self(id, PhantomData))
+        }
     }
 }
