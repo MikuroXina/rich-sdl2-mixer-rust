634f4629be199ff027df6276fbf2bbde5065adf9
MikuroXina <MikuroXina@users.noreply.github.com>
2021-09-23 15:53:41 +0000

    deploy: 72a356c69e3d9b8ee9bae02428f944b794683ca8

--- build.rs
+++ build.rs
@@ -2,11 +2,42 @@ fn main() {
     use git2::Repository;
     use std::env;
     use std::path::PathBuf;
+    use std::process;
 
-    let _ = Repository::clone("https://github.com/libsdl-org/SDL", "SDL2");
-    let _ = Repository::clone("https://github.com/libsdl-org/SDL_mixer", "SDL2_mixer");
+    let root_dir = env::var("OUT_DIR").expect("OUT_DIR not found");
+    let root = PathBuf::from(&root_dir);
 
-    let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not found"));
+    let sdl_dir = root.join("SDL2");
+    let _ = Repository::clone("https://github.com/libsdl-org/SDL", &sdl_dir);
+    let _ = process::Command::new("./configure")
+        .arg(format!("--prefix={}", root_dir))
+        .current_dir(&sdl_dir)
+        .output()
+        .expect("failed to configure");
+    let _ = process::Command::new("make")
+        .arg("-j4")
+        .current_dir(&sdl_dir)
+        .output()
+        .expect("failed to make");
+    let _ = process::Command::new("make")
+        .arg("install")
+        .current_dir(&sdl_dir)
+        .output()
+        .expect("failed to install");
+
+    let sdl_mixer_dir = root.join("SDL2_mixer");
+    let _ = Repository::clone("https://github.com/libsdl-org/SDL_mixer", &sdl_mixer_dir);
+    let _ = process::Command::new("./configure")
+        .arg(format!("--prefix={}", root_dir))
+        .current_dir(&sdl_mixer_dir)
+        .env("SDL2_DIR", &sdl_dir)
+        .output()
+        .expect("failed to configure");
+    let _ = process::Command::new("make")
+        .arg("-j4")
+        .current_dir(&sdl_mixer_dir)
+        .output()
+        .expect("failed to make");
 
     println!("cargo:rustc-link-lib=SDL2");
     println!("cargo:rustc-link-lib=SDL2_mixer");
@@ -24,20 +55,18 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-ISDL2/include")
+        .clang_arg(&format!("-I{}/SDL2/include", root_dir))
+        .clang_arg(&format!("-I{}/SDL2_mixer/include", root_dir))
         .allowlist_function("Mix_.*")
         .allowlist_function("SDL_RWFromFile")
         .allowlist_type("MIX_.*")
         .allowlist_var("MIX_.*")
         .generate_comments(false)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .raw_line("//! Rust FFI to `SDL_mixer.h`")
-        .raw_line("")
-        .raw_line(r"#![allow(warnings)]")
         .generate()
         .expect("bindgen builder was invalid");
 
     bindings
-        .write_to_file(root.join("src/bind.rs"))
+        .write_to_file(root.join("bind.rs"))
         .expect("`src` directory not found");
 }
